f71853d61ced8d58a740ea520715ab6b158a9529
Rahul Bansod <rahulbansod519@gmail.com>
2022-04-15 16:12:31 +0530

    Add files via upload

---
+++ main_app.py
@@ -0,0 +1,84 @@
+import os
+import cv2 
+import numpy as np
+import tensorflow as tf
+from object_detection.utils import label_map_util
+from object_detection.utils import config_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from ocr_detection import ocr_it,save_results
+WORKSPACE_PATH = 'Tensorflow/workspace'
+SCRIPTS_PATH = 'Tensorflow/scripts'
+APIMODEL_PATH = 'Tensorflow/models'
+ANNOTATION_PATH = WORKSPACE_PATH+'/annotations'
+IMAGE_PATH = WORKSPACE_PATH+'/images'
+MODEL_PATH = WORKSPACE_PATH+'/models'
+PRETRAINED_MODEL_PATH = WORKSPACE_PATH+'/pre-trained-models'
+CONFIG_PATH = MODEL_PATH+'/my_ssd_mobnet/pipeline.config'
+CHECKPOINT_PATH = MODEL_PATH+'/my_ssd_mobnet/'
+# Load pipeline config and build a detection model
+
+configs = config_util.get_configs_from_pipeline_file(MODEL_PATH+'/'+'my_ssd_mobnet'+'/pipeline.config')
+detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+
+# Restore checkpoint
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore(os.path.join(CHECKPOINT_PATH, 'ckpt-14')).expect_partial()
+region_threshold = 0.6
+detection_threshold = 0.7
+@tf.function
+def detect_fn(image):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+    return detections
+
+category_index = label_map_util.create_category_index_from_labelmap(ANNOTATION_PATH+'/label_map.pbtxt')
+# Setup capture
+# cap = cv2.VideoCapture('sample.mp4')
+cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture('https://192.168.137.251:8080/video')
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while cap.isOpened(): 
+    
+    ret, frame = cap.read()
+    image_np = np.array(frame)
+    
+    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+    detections = detect_fn(input_tensor)
+    # print(detections)
+    
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    label_id_offset = 1
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+                image_np_with_detections,
+                detections['detection_boxes'],
+                detections['detection_classes']+label_id_offset,
+                detections['detection_scores'],
+                category_index,
+                use_normalized_coordinates=True,
+                max_boxes_to_draw=5,
+                min_score_thresh=.8,
+                agnostic_mode=False)
+
+    text = ocr_it(image_np_with_detections, detections, detection_threshold, region_threshold)
+    save_results('realtimeresults.csv',text)
+      
+    cv2.imshow('object detection',  cv2.resize(image_np_with_detections, (800, 600)))
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cap.release()
+        break
+
+    
